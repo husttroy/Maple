2483d30407e74979985dea6750778e66bc6dc97c
husttroy <troyathust@gmail.com>
2017-02-02 21:15:47 -0800

    add two python scripts to do frequent itemset mining and frequent sequence mining.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Slicer
+
+Slicer is highly extensible. Slicer uses the Strategy design pattern to register data processing mechanisms for BOA output files. Slicer can be easily augmented to process new types of BOA outputs by extending the ProcessStrategy class.
+
+Run the scripts in CodeMiner, download BOA outputs, and put all txt files in the example folder.
+
+To mine patterns from API call sequences, please install pymining first using 'pip install pymining'.
---
+++ src/edu/ucla/cs/slice/PatternVerifier.java
@@ -0,0 +1,78 @@
+package edu.ucla.cs.slice;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class PatternVerifier {
+	
+	public ArrayList<String> pattern;
+	
+	public PatternVerifier() {
+		pattern = new ArrayList<String>();
+		pattern.add("new File");
+		pattern.add("exists");
+		pattern.add("IF {");
+		pattern.add("createNewFile");
+		pattern.add("}");
+	}
+	
+	public ArrayList<ArrayList<String>> readAPISeqeunces(String path){
+		ArrayList<ArrayList<String>> seqs = new ArrayList<ArrayList<String>>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(new File(path)))){
+			String line;
+			while((line = br.readLine()) != null) {
+				if(line.contains("---")){
+					String s = line.split("---")[1];
+					s = s.substring(1, s.length() - 1);
+					ArrayList<String> seq = new ArrayList<String>();
+					for(String api : s.split(",")){
+						seq.add(api.trim());
+					}
+					seqs.add(seq);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return seqs;
+	}
+	
+	public void verify(String path){
+		ArrayList<ArrayList<String>> seqs = readAPISeqeunces(path);
+		ArrayList<String> temp = new ArrayList<String>(pattern);
+		int support = 0;
+		for(ArrayList<String> seq : seqs){
+			for(String api : seq){
+				String s = temp.get(0);
+				if(api.equals(s)) {
+					temp.remove(0);
+					if(temp.isEmpty()) {
+						support ++;
+						break;
+					}
+				}
+			}
+			
+			// reset
+			temp.clear();
+			temp.addAll(pattern);
+		}
+		
+		System.out.println(support);
+	}
+	
+	public static void main(String[] args){
+		String output = "/home/troy/research/BOA/Slicer/example/output.txt";
+		PatternVerifier pv = new PatternVerifier();
+		pv.verify(output);
+	}
+
+}
