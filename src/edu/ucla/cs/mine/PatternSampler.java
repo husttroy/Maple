35f5ecd997addaa5bb11d604502a969d48a9bf9a
husttroy <troyathust@gmail.com>
2017-07-11 23:40:43 -0700

    Optimize the ranking and selecting process.

--- src/edu/ucla/cs/mine/PatternSampler.java
+++ src/edu/ucla/cs/mine/PatternSampler.java
@@ -16,6 +16,7 @@ import java.util.Map.Entry;
 import edu.ucla.cs.model.APICall;
 import edu.ucla.cs.model.APISeqItem;
 import edu.ucla.cs.utils.SAT;
+import edu.ucla.cs.utils.SubsequenceCounter;
 
 public class PatternSampler {
 	public String seqFile;
@@ -39,12 +40,61 @@ public class PatternSampler {
 		TraditionalPredicateMiner minerP = new TraditionalPredicateMiner(
 				patternS, orgFile, seqFile);
 		minerP.loadAndFilterPredicate();
+		
+		// rank code examples that support the sequence ordering based on their
+		// conciseness (i.e., number of API calls and control constructs in a
+		// sequence) and their ambiguity (i.e., whether there is an n-n mapping
+		// between items in the pattern and items in the sequence)
+		List<Map.Entry<String, ArrayList<String>>> sortedList = new LinkedList<Map.Entry<String, ArrayList<String>>>(verifyS.support.entrySet());
+		
+		Collections.sort(sortedList, new Comparator<Map.Entry<String, ArrayList<String>>>() {
+
+			@Override
+			public int compare(Entry<String, ArrayList<String>> o1,
+					Entry<String, ArrayList<String>> o2) {
+				// first we prefer non-ambiguous examples
+				ArrayList<String> seq1 = o1.getValue();
+				ArrayList<String> seq2 = o2.getValue();
+				if (!isAmbiguous(patternS, seq1) && isAmbiguous(patternS, seq2)) {
+					return -1;
+				} else if (isAmbiguous(patternS, seq1) && !isAmbiguous(patternS, seq2)) {
+					return 1;
+				}
+				
+				if (seq1.size() < seq2.size()) {
+					return -1;
+				} else if (seq1.size() > seq2.size()) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+
+			private boolean isAmbiguous(ArrayList<String> pattern,
+					ArrayList<String> seq) {
+				// make a copy of seq
+				ArrayList<String> copy = new ArrayList<String>(seq);
+				
+				// strip all items in value that are not in pattern to reduce complexity
+				for(int i = copy.size() -1; i > 0; i--) {
+					if(!pattern.contains(copy.get(i))) {
+						copy.remove(i);
+					}
+				}
+				
+				SubsequenceCounter counter = new SubsequenceCounter(copy, pattern);
+				return counter.countMatches() > 1;
+			}
+			
+		});
 
 		// get ids of all code examples that respect the sequence ordering and the guard condition
-		ArrayList<String> supportIDs = new ArrayList<String>();
+		ArrayList<String> sampleIDs = new ArrayList<String>();
 		SAT sat = new SAT();
-		for (String id : verifyS.support.keySet()) {
+		for (Map.Entry<String, ArrayList<String>> entry : sortedList) {
+			String id = entry.getKey();
 			HashMap<String, ArrayList<String>> map = minerP.predicates.get(id);
+			if(map == null)  continue;
 			boolean flag = true;
 			for (APISeqItem item : pattern) {
 				if (!flag) {
@@ -71,64 +121,11 @@ public class PatternSampler {
 			}
 
 			if (flag) {
-				supportIDs.add(id);
-			}
-		}
-		
-		// rank code examples that support the sequence ordering based on their
-		// conciseness (i.e., number of API calls and control constructs in a
-		// sequence) and their ambiguity (i.e., whether there is an n-n mapping
-		// between items in the pattern and items in the sequence)
-		List<Map.Entry<String, ArrayList<String>>> sortedList = new LinkedList<Map.Entry<String, ArrayList<String>>>();
-		
-		// add all supported examples to the list
-		for(Entry<String, ArrayList<String>> e : verifyS.support.entrySet()) {
-			if(supportIDs.contains(e.getKey())) {
-				sortedList.add(e);
-			}
-		}
-		
-		Collections.sort(sortedList, new Comparator<Map.Entry<String, ArrayList<String>>>() {
-
-			@Override
-			public int compare(Entry<String, ArrayList<String>> o1,
-					Entry<String, ArrayList<String>> o2) {
-				// first we prefer non-ambiguous examples
-				ArrayList<String> seq1 = o1.getValue();
-				ArrayList<String> seq2 = o2.getValue();
-				if (!isAmbiguous(patternS, seq1) && isAmbiguous(patternS, seq2)) {
-					return -1;
-				} else if (isAmbiguous(patternS, seq1) && !isAmbiguous(patternS, seq2)) {
-					return 1;
-				}
-				
-				if (seq1.size() < seq2.size()) {
-					return -1;
-				} else if (seq1.size() > seq2.size()) {
-					return 1;
-				} else {
-					return 0;
+				sampleIDs.add(id);
+				if(sampleIDs.size() == n) {
+					break;
 				}
 			}
-
-			private boolean isAmbiguous(ArrayList<String> pattern,
-					ArrayList<String> value) {
-				for(String item : pattern) {
-					if(item.equals("}") && Collections.frequency(value, item) > 1) {
-						return true;
-					}
-				}
-				
-				return false;
-			}
-			
-		});
-		
-		// get the IDs for the top 10 examples
-		List<String> topN = new ArrayList<String>();
-		n = sortedList.size() > n ? n : sortedList.size();
-		for(int i = 0; i < n; i ++) {
-			topN.add(sortedList.get(i).getKey());
 		}
 
 		// read the original file again to get the complete sequence of each
@@ -148,8 +145,8 @@ public class PatternSampler {
 				String key = line.substring(line.indexOf("[") + 1,
 						line.indexOf("][SEQ]"));
 				key = key.replaceAll("\\!", " ** ");
-				if (topN.contains(key)) {
-					sample.add(topN.indexOf(key), line);
+				if (sampleIDs.contains(key)) {
+					sample.add(sampleIDs.indexOf(key), line);
 				}
 			}
 		} catch (IOException e) {
@@ -157,7 +154,7 @@ public class PatternSampler {
 		}
 		
 		// post-process, remove all nulls from the sample list
-		for(int i = sample.size() - 1; i > 0; i--) {
+		for(int i = sample.size() - 1; i > -1; i--) {
 			if(sample.get(i) == null) {
 				sample.remove(i);
 			}
